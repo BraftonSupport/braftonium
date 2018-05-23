6b37f0b7986db01909f0fc6337b1989b6926dfce
Yvonne Tse <yvonne.tse@brafton.com>
2018-05-23 14:33:26 -0400

    fixy fixy

--- library/custom-fields/fields.php
+++ library/custom-fields/fields.php
@@ -14,13 +14,13 @@ if(function_exists("register_field_group"))
 		}
 	}
 		
-	if ( in_array('testimonial', $custom_post_types) ) {
+	if ( is_array($custom_post_types) && in_array('testimonial', $custom_post_types) ) {
 		require_once get_template_directory().'/library/custom-post-types/testimonials/testimonials.acf.php';
 	}
-	if(in_array('event', $custom_post_types) ) {
+	if ( is_array($custom_post_types) &&  in_array('event', $custom_post_types) ) {
 		require_once get_template_directory(). '/library/custom-post-types/events/events.acf.php';
 	}
-	if( in_array('team_member', $custom_post_types) ) {
+	if ( is_array($custom_post_types) &&  in_array('team_member', $custom_post_types) ) {
 		require_once get_template_directory(). '/library/custom-post-types/team/team.acf.php';
 	}
 }
