73c76e98b45dc6dbe533bf999608c1b13d86b9d9
Yvonne Tse <yvonne.tse@brafton.com>
2018-10-23 13:28:23 -0400

    fixed #content>#inner-content>main order

--- archive.php
+++ archive.php
@@ -8,7 +8,7 @@ $layoutarray = array('full','rich','simple');
 			<div id="inner-content" class="wrap cf">
 				<main id="main" class="m-all <?php if(is_active_sidebar('blog-sidebar')): echo 't-2of3 d-5of7'; endif; ?> cf<?php echo ' '.$layout; ?>" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-					<?php if (!isset($background_image)):
+					<?php if (!$background_image):
 						the_archive_title( '<h1 class="page-title">', '</h1>' );
 						the_archive_description( '<div class="taxonomy-description">', '</div>' );
 					endif; ?>
--- page.php
+++ page.php
@@ -4,57 +4,47 @@ $brafton_comments = get_field('comments', 'option');
 ?>
 
 	<div id="content">
-		<div id="inner-content" class="cf">
+		<div id="inner-content" class="wrap cf">
+			<main id="main" class="m-all <?php if ( is_active_sidebar( 'page-sidebar' ) ) : echo 't-2of3 d-5of7 '; endif; ?>cf" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+			
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-			<main id="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-				
-				<div class="wrap">
-					<div class="m-all <?php if ( is_active_sidebar( 'page-sidebar' ) ) : echo 't-2of3 d-5of7 '; endif; ?>cf" >
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> itemscope itemtype="http://schema.org/BlogPosting">
 
-						<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> itemscope itemtype="http://schema.org/BlogPosting">
+						<?php if (!$title): ?><header class="article-header">
+							<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+						</header><?php endif;// end article header ?>
 
-							<?php if (!$title): ?><header class="article-header">
-								<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-							</header><?php endif;// end article header ?>
+						<section class="entry-content cf" itemprop="articleBody">
+							<?php
+								// the content (pretty self explanatory huh)
+								the_content();
 
-							<section class="entry-content cf" itemprop="articleBody">
-								<?php
-									// the content (pretty self explanatory huh)
-									the_content();
-
-									// If comments are open or we have at least one comment, load up the comment template.
-									if ( !empty($brafton_comments) && comments_open() || get_comments_number() ) :
-										comments_template();
-									endif;
-								?>
-							</section> <?php // end article section 
-							 	$defaults = array(
-									'before'           => '<p>' . __( 'Pages:', 'braftonium' ),
-									'after'            => '</p>',
-									'link_before'      => '',
-									'link_after'       => '',
-									'next_or_number'   => 'number',
-									'separator'        => ' ',
-									'nextpagelink'     => __( 'Next page', 'braftonium'),
-									'previouspagelink' => __( 'Previous page', 'braftonium' ),
-									'pagelink'         => '%',
-									'echo'             => 1
-								);
-								 
-									wp_link_pages( $defaults );
+								// If comments are open or we have at least one comment, load up the comment template.
+								if ( !empty($brafton_comments) && comments_open() || get_comments_number() ) :
+									comments_template();
+								endif;
 							?>
-
-						</article>
-
-					<?php endwhile; endif; ?>
-
-
-				
-				</div>
-				<?php get_sidebar(); ?>
-			</div>
+						</section> <?php // end article section 
+						 	$defaults = array(
+								'before'           => '<p>' . __( 'Pages:', 'braftonium' ),
+								'after'            => '</p>',
+								'link_before'      => '',
+								'link_after'       => '',
+								'next_or_number'   => 'number',
+								'separator'        => ' ',
+								'nextpagelink'     => __( 'Next page', 'braftonium'),
+								'previouspagelink' => __( 'Previous page', 'braftonium' ),
+								'pagelink'         => '%',
+								'echo'             => 1
+							);
+								 
+							wp_link_pages( $defaults );
+						?>
+					</article>
+				<?php endwhile; endif; ?>
 			</main>
+			<?php get_sidebar(); ?>
 		</div>
 	</div>
 <?php get_footer(); ?>
