79492dfd29ef33c3cfb23234ce8aad1a54316c18
Yvonne Tse <yvonne.tse@brafton.com>
2018-12-10 12:35:18 -0500

    fixing for wp 5

--- functions.php
+++ functions.php
@@ -1,4 +1,14 @@
 <?php
+// Disable Gutenberg
+
+if (version_compare($GLOBALS['wp_version'], '5.0-beta', '>')) {
+	// WP > 5 beta
+	add_filter('use_block_editor_for_post_type', '__return_false', 10);
+} else {
+	// WP < 5 beta
+	add_filter('gutenberg_can_edit_post_type', '__return_false', 10);
+}
+
 
 /* Languages! */
 function braftonium_language_setup(){
