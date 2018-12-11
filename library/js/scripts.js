e83d4b0e37cd92cffbbe02787dc0096ca10e44af
Yvonne Tse <yvonne.tse@brafton.com>
2018-12-11 13:46:13 -0500

    update to fix js

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -4,8 +4,8 @@
  * ( source: http://andylangton.co.uk/blog/development/get-viewport-size-width-and-height-javascript )
 */
 function updateViewportDimensions() {
-	var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
-	return { width:x,height:y };
+  var w = window, d = document, e = d.documentElement, g = d.getElementsByTagName('body')[0], x = w.innerWidth || e.clientWidth || g.clientWidth, y = w.innerHeight || e.clientHeight || g.clientHeight;
+  return { width: x, height: y };
 }
 // setting the viewport width
 var viewport = updateViewportDimensions();
@@ -20,9 +20,9 @@ function loadGravatars() {
   viewport = updateViewportDimensions();
   // if the viewport is tablet or larger, we load in the gravatars, rotate buttons and show the menu button
   if (viewport.width >= 768) {
-  // set the viewport using the function above
-    jQuery('.comment img[data-gravatar]').each(function(){
-      jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
+    // set the viewport using the function above
+    jQuery('.comment img[data-gravatar]').each(function () {
+      jQuery(this).attr('src', jQuery(this).attr('data-gravatar'));
     });
   }
 } // end function
@@ -31,26 +31,26 @@ function rotateButtons() {
   viewport = updateViewportDimensions();
   if (viewport.width <= 768) {
     buttons = document.querySelectorAll('#inner-header nav button');
-    buttons.forEach(function(button) {
-      if (button.classList){
+    buttons.forEach(function (button) {
+      if (button.classList) {
         button.classList.add('rotate');
       }
     });
   } else {
     navButtons = document.querySelectorAll('header nav button');
-    navButtons.forEach(function(navButton) {
-      navButton.addEventListener('click',function() {
+    navButtons.forEach(function (navButton) {
+      navButton.addEventListener('click', function () {
         if (navButton.classList) {
           navButton.classList.toggle('rotate');
         } else {
           var navClasses = navButton.className.split(' ');
           var navExistingIndex = classes.indexOf('rotate');
-        
+
           if (navExistingIndex >= 0)
             navClasses.splice(navExistingIndex, 1);
           else
             navClasses.push('rotate');
-        
+
           navButton.className = navClasses.join(' ');
         }
         subMenu = navButton.nextElementSibling;
@@ -59,13 +59,13 @@ function rotateButtons() {
         } else {
           var subMenuClasses = subMenu.className.split(' ');
           var subMenuExistingIndex = classes.indexOf('toggleon');
-          
-          if (subMenuExistingIndex >= 0){
+
+          if (subMenuExistingIndex >= 0) {
             subMenuClasses.splice(subMenuExistingIndex, 1);
           } else {
             subMenuClasses.push('toggleon');
           }
-        subMenu.className = subMenuClasses.join(' ');
+          subMenu.className = subMenuClasses.join(' ');
         }
       });
     });
@@ -74,55 +74,54 @@ function rotateButtons() {
 
 function menuToggle() {
   viewport = updateViewportDimensions();
-  if (viewport.width <= 768) {
-    //The menu button in the mobile version
-    menuToggleButton = document.getElementById('menu-toggle');
-    //when clicked toggles class "toggledon" on the nav
-    menuToggleButton.addEventListener('click',function() {
-      nav = document.querySelector('nav');
-      if (nav.classList) {
-        nav.classList.toggle('toggleon');
-      } else {
-        var classes = nav.className.split(' ');
-        var existingIndex = classes.indexOf('toggleon');
-      
-        if (existingIndex >= 0)
-          classes.splice(existingIndex, 1);
-        else
-          classes.push('toggleon');
-      
-        el.className = classes.join(' ');
+
+  //The menu button in the mobile version
+  menuToggleButton = document.getElementById('menu-toggle');
+  //when clicked toggles class "toggledon" on the nav
+  menuToggleButton.addEventListener('click', function () {
+    nav = document.querySelector('nav');
+    if (nav.classList) {
+      nav.classList.toggle('toggleon');
+    } else {
+      var classes = nav.className.split(' ');
+      var existingIndex = classes.indexOf('toggleon');
+
+      if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+      else
+        classes.push('toggleon');
+
+      el.className = classes.join(' ');
+    }
+
+    //also switches the svg from menu to close
+    svgs = document.querySelectorAll('#menu-toggle svg');
+    svgs.forEach(function (x) {
+      if (x.style.display === "inline") {
+        x.style.display = "none";
+      } else if (x.style.display === "none") {
+        x.style.display = "inline";
       }
-
-      //also switches the svg from menu to close
-      svgs = document.querySelectorAll('#menu-toggle svg');
-      svgs.forEach(function(x) {
-        if (x.style.display === "inline") {
-          x.style.display = "none";
-        } else if (x.style.display === "none"){
-          x.style.display = "inline";
-        }
-      });
-
     });
-  }
+  });
 }
+
 function smoothScroll() {
   viewport = updateViewportDimensions();
   if (viewport.width <= 768) {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       anchor.addEventListener('click', function (e) {
-          e.preventDefault();
+        e.preventDefault();
 
-          document.querySelector(this.getAttribute('href')).scrollIntoView({
-              behavior: 'smooth'
-          });
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
       });
     });
   }
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   loadGravatars();
   rotateButtons();
   document.querySelector('#menu-toggle .icon-close').style.display = "none";
